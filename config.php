1c19c0af81a34a4475a43848522d8b164d83b27a
Marko <michinaux.marko@gmail.com>
2024-10-10 16:29:24 -0400

    verbose description in config

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 
-define('API_KEY', ''); # Do not add the API_KEY into your commits, incur in violation.
-define('BASE_URL', 'https://api.hubapi.com/crm/v3/'); # modify endpoint as required
+define('API_KEY', ''); # Do not add this file with a populated API_KEY string as changes into your commits, incur in violation.
+define('BASE_URL', ''); # modify endpoint as required
 define('ENABLE_DEBUG', true);
-define('VICIDIAL_USER', 'APIUSER');
+# change credentials and URL as required. By default these are used.
+define('VICIDIAL_USER', 'APIUSER'); 
 define('VICIDIAL_PASS', 'C6Dlr03ceuvJzGg9V1');
 define('DIALER_URL', 'https://login.theccdocs.com');
